a968be3c6c84d656acd56ff5486ce88a1c92ef78
PakkuDon <pakku.pd@gmail.com>
2015-05-28 23:42:07 +1000

    Added a button to hide/show player controls/details

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Register event handlers
+// Initialise components and register event handlers
 window.onload = function(e) {
     // Get required elements and create Player instance
     var audioElement = document.getElementById("player");
@@ -10,10 +10,25 @@ window.onload = function(e) {
     var player = new Player(audioElement, canvas,
                             thumbnail, artist, title);
 
+    // Load and play song on submit
     var form = document.getElementById("player-form");
     form.onsubmit = function(e) {
         e.preventDefault();
         var url = document.getElementById("track-url").value;
         player.play(url);
+    }
+
+    // Hide/show form
+    var toggleFormBtn = document.getElementById("toggle-form-btn");
+    toggleFormBtn.onclick = function(e) {
+        var container = document.getElementById("collapsible-container");
+        if (container.style.display === "none") {
+            container.style.display = "block";
+            toggleFormBtn.innerHTML = "&#9660;";
+        }
+        else {
+            container.style.display = "none";
+            toggleFormBtn.innerHTML = "&#9650;";
+        }
     }
 };
