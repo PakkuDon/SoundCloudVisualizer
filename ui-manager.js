712562bd61b5217ec6e7ee6dfde5a326f3016625
PakkuDon <pakku.pd@gmail.com>
2015-05-31 23:20:51 +1000

    Added an error message if browser agent doesn't support Web Audio API
    - Added UIManager.showNotification() method.
    - Added check to index.js to see if AudioContext was supported.
    - Added styling rules for notifications.

--- ui-manager.js
+++ ui-manager.js
@@ -20,4 +20,19 @@ function UIManager(container, notifications, thumbnail, artist, title, genre) {
             e.target.innerHTML = "&#9650;";
         }
     }
+
+    // Adds a message to the designated notifications container
+    this.showNotification = function(header, message) {
+        var notification = document.createElement("div");
+        notification.className = "message";
+
+        var headerElement = document.createElement("div");
+        headerElement.innerHTML = header;
+        var messageElement = document.createElement("div");
+        messageElement.innerHTML = message;
+
+        notification.appendChild(headerElement);
+        notification.appendChild(messageElement);
+        notifications.appendChild(notification);
+    }
 }
