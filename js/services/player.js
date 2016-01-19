eb38e69680dcf5b210bf4f1712d437e5944dcbbe
PakkuDon <pakku.pd@gmail.com>
2016-01-19 23:57:57 +1100

    Added player service

---
+++ js/services/player.js
@@ -0,0 +1,41 @@
+angular.module('scvServices').factory('player', 
+['soundcloudResolver', function(soundcloudResolver) {
+    function Player() {
+        var self = this;
+        var history = [];
+        var playbackQueue = [];
+        var currentTrack = {};
+        
+        this.play = function(trackUrl) {
+            // Retrieve track 
+            soundcloudResolver.load(trackUrl, function(track) {
+                // If selected track already in history, remove it
+                for (var i = 0; i < history.length; i++) {
+                    if (history[i].id == track.id) {
+                        history.splice(i, 1);
+                        break;
+                    }
+                }
+                
+                // Play song and add to history
+                currentTrack = track;
+                history.push(track);
+            });
+        }
+        
+        this.getHistory = function() {
+            return history;
+        }
+        
+        this.getPlaybackQueue = function() {
+            return playbackQueue;
+        }
+        
+        this.getCurrentTrack = function() {
+            return currentTrack;
+        }
+    }
+    
+    var player = new Player();
+    return player;
+}]);
