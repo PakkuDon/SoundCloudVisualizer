2e1ff6793442b7e5d0d145bd3606c67be03319c4
PakkuDon <pakku.pd@gmail.com>
2016-01-22 23:28:56 +1100

    Added Playback Queue to sidebar
    - Added playback list under history.
    - Added play and delete functions to PlaybackQueueController.

--- js/controllers/PlaybackQueueController.js
+++ js/controllers/PlaybackQueueController.js
@@ -1,3 +1,13 @@
-angular.module('scvControllers').controller('PlaybackQueueController', function() {
+angular.module('scvControllers').controller('PlaybackQueueController', 
+['player', function(player) {
+    this.player = player;
     
-});
+    this.play = function(index) {
+        var track = this.player.getPlaybackQueue().splice(index, 1)[0];
+        player.play(track.permalink_url);
+    }
+    
+    this.delete = function(index) {
+        this.player.getPlaybackQueue().splice(index, 1);
+    }
+}]);
