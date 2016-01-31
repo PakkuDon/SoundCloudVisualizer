2634463f51d77c4a947814299e4d46c449d5d269
PakkuDon <pakku.pd@gmail.com>
2016-01-31 19:08:07 +1100

    Removed unused files
    - Deleted old JavaScript code.

--- js/services/soundcloudResolver.js
+++ js/services/soundcloudResolver.js
@@ -1,7 +1,7 @@
 angular.module('scvServices').factory('soundcloudResolver', ['$http', function($http) {
     var clientID = '4db236383438b2ebbe8e4f151e1c1b59';
     SC.initialize({
-        client_id: API.clientID
+        client_id: clientID
     });
     
     // Retrieve track data via SoundCloud API
