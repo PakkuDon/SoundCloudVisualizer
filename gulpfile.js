4fee66b153c4d2cccb4e4189c278c413c8f9ccbb
PakkuDon <pakku.pd@gmail.com>
2016-02-23 23:57:15 +1100

    Added gulpfile
    - Added gulp, gulp-concat and gulp-uglify dev dependencies.
    - Added a task to minify vendor files.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('vendor', function() {
+    return gulp.src([
+        'bower_components/jquery/dist/jquery.min.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-sanitize/angular-sanitize.min.js'
+    ]).pipe(concat('vendor.js'))
+    .pipe(uglify({ mangle: false }))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('default', ['vendor']);
