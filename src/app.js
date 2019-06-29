9afd5d3482e53d6a3caa9547b82e735d028043cd
PakkuDon <pakku.pd@gmail.com>
2019-06-29 23:55:46 +1000

    Create root component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './style.css'
+
+const App = () => (
+  <div>Hello World</div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#app'),
+)
