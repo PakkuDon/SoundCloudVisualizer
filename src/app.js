472927463b896214c365549bf76675db85bb8523
PakkuDon <pakku.pd@gmail.com>
2019-06-29 23:50:07 +1000

    Remove code bootstrapping Angular modules

--- src/app.js
+++ src/app.js
@@ -1,9 +1,1 @@
 import './style.css'
-
-var controllers = angular.module('scvControllers', []);
-var services = angular.module('scvServices', ['playerServices']);
-var playerServices = angular.module('playerServices', []);
-var filters = angular.module('scvFilters', ['ngSanitize']);
-var directives = angular.module('scvDirectives', []);
-var app = angular.module('SoundCloudVisualizer',
-    ['scvServices', 'scvControllers', 'scvFilters', 'scvDirectives']);
