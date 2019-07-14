ee92d27d0d49797c21c2369797d94415d0a774d3
PakkuDon <pakku.pd@gmail.com>
2019-07-15 00:35:24 +1000

    Add component to display notifications

---
+++ src/components/NotificationList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import IconDelete from './IconDelete'
+
+const NotificationList = ({ notifications }) => (
+  <React.Fragment>
+    {notifications.map(notification => (
+      <div key={notification} className="message">
+        <span>{notification}</span>
+        <button className="delete" title="Delete message">
+          <IconDelete />
+        </button>
+      </div>
+    ))}
+  </React.Fragment>
+)
+
+NotificationList.propTypes = {
+  notifications: PropTypes.arrayOf(PropTypes.string),
+}
+
+NotificationList.defaultProps = {
+  notifications: [],
+}
+
+export default NotificationList
