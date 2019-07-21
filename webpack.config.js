f3f9772f341ca68c4191d3326ceced8a52afd7ed
PakkuDon <pakku.pd@gmail.com>
2019-07-21 13:57:52 +1000

    Enable CSS modules

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,12 @@ module.exports = {
           },
           {
             loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
+            },
           },
         ],
       },
