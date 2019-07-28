6c41e69149fb8991dd1a0ad7999130cdfa328263
PakkuDon <pakku.pd@gmail.com>
2019-07-28 23:56:53 +1000

    Use CSS modules in <NotificationList />

--- src/components/NotificationList/index.js
+++ src/components/NotificationList/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import IconDelete from '../icons/IconDelete'
+import styles from './styles.css'
 
 const NotificationList = ({ notifications }) => (
   <React.Fragment>
     {notifications.map(notification => (
-      <div key={notification} className="message">
+      <div key={notification} className={styles.root}>
         <span>{notification}</span>
-        <button className="delete" title="Delete message">
+        <button className={styles.delete} title="Delete message">
           <IconDelete />
         </button>
       </div>
