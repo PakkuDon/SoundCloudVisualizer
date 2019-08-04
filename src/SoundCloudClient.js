14b555b7c030418bfcc2820c03a75da344ae20e8
PakkuDon <pakku.pd@gmail.com>
2019-08-04 23:23:37 +1000

    Export SoundCloud client ID from SoundCloudClient module
    This needs to be appended to the stream URL before we can play it

--- src/SoundCloudClient.js
+++ src/SoundCloudClient.js
@@ -1,6 +1,6 @@
 import SoundCloudSDK from 'soundcloud'
 
-const SOUNDCLOUD_CLIENT_ID = '4db236383438b2ebbe8e4f151e1c1b59'
+export const SOUNDCLOUD_CLIENT_ID = '4db236383438b2ebbe8e4f151e1c1b59'
 
 SoundCloudSDK.initialize({
   client_id: SOUNDCLOUD_CLIENT_ID,
