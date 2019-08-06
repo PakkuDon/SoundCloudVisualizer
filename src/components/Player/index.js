a6d8a1ab02e3949915347e3af0c82ece7deea66d
PakkuDon <pakku.pd@gmail.com>
2019-08-07 00:01:07 +1000

    Resolve song on Player form submit, not just on button click
    Allows user to select song by hitting Enter on track input

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -4,35 +4,42 @@ import Button from '../Button'
 import { SoundCloudTrack } from '../../propTypes'
 import styles from './styles.css'
 
-const Player = ({ inputUrl, onSongSelect, onUrlEdit, track }) => (
-  <div className={styles.root}>
-    <div>
-      <form>
-        <input
-          type="text"
-          size="40"
-          value={inputUrl}
-          onChange={(event) => onUrlEdit(event.target.value)}
-        />
-      <Button onClick={onSongSelect}>Load song</Button>
-      </form>
-    </div>
-    <div>
-      <img className={styles.thumbnail} src={track.artwork_url} />
-      <div className={styles.details}>
-        <div>{track.user && track.user.username}</div>
-        <div>{track.title}</div>
-        <div>{track.genre}</div>
+const Player = ({ inputUrl, onSongSelect, onUrlEdit, track }) => {
+  const onSubmit = (event) => {
+    event.preventDefault()
+    onSongSelect()
+  }
+
+  return (
+    <div className={styles.root}>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            size="40"
+            value={inputUrl}
+            onChange={(event) => onUrlEdit(event.target.value)}
+          />
+          <Button type="submit">Load song</Button>
+        </form>
+      </div>
+      <div>
+        <img className={styles.thumbnail} src={track.artwork_url} />
+        <div className={styles.details}>
+          <div>{track.user && track.user.username}</div>
+          <div>{track.title}</div>
+          <div>{track.genre}</div>
+        </div>
+      </div>
+      <audio autoPlay crossOrigin="anonymous" controls src={track.stream_url} >
+        Your browser does not support the HTML5 audio element.
+      </audio>
+      <div className={styles.footer}>
+        <a href="https://github.com/PakkuDon/SoundCloudVisualizer">Source code on GitHub</a>
       </div>
     </div>
-    <audio autoPlay crossOrigin="anonymous" controls src={track.stream_url} >
-      Your browser does not support the HTML5 audio element.
-    </audio>
-    <div className={styles.footer}>
-      <a href="https://github.com/PakkuDon/SoundCloudVisualizer">Source code on GitHub</a>
-    </div>
-  </div>
-)
+  )
+}
 
 Player.propTypes = {
   inputUrl: PropTypes.string,
