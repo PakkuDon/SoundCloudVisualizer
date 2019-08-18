097422b731da3b07c05e16ac4392cd068e19ff1a
PakkuDon <pakku.pd@gmail.com>
2019-08-18 23:31:36 +1000

    Convert <Visualizer /> to a class component

--- src/components/Visualizer/index.js
+++ src/components/Visualizer/index.js
@@ -2,20 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.css'
 
-const Visualizer = (props) => (
-  <React.Fragment>
-    <div className={styles.root}>
-      <canvas className={styles.canvas} width="500" height="500">
-        Your browser does not support the HTML5 canvas element.
-      </canvas>
-    </div>
-    <div className={styles.options}>
-      <label>
-        Visualizer: <select></select>
-      </label>
-    </div>
-  </React.Fragment>
-)
+class Visualizer extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className={styles.root}>
+          <canvas className={styles.canvas} width="500" height="500">
+            Your browser does not support the HTML5 canvas element.
+          </canvas>
+        </div>
+        <div className={styles.options}>
+          <label>
+            Visualizer: <select></select>
+          </label>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
 
 Visualizer.propTypes = {
 
