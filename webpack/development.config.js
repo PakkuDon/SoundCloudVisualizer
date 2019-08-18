eac21eedabea8226f291875d2fd06bea66499129
PakkuDon <pakku.pd@gmail.com>
2019-08-18 21:57:06 +1000

    Fix output path
    This config file used to live in the project root. We've moved it down
    one level but the bundle should still be output to root

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js',
   },
   devServer: {
