c20683c5d022fd7e31a68794cd6815c8816c3946
PakkuDon <pakku.pd@gmail.com>
2019-08-25 21:57:37 +1000

    Initialize canvas rendering context in <Visualizer />

--- src/components/Visualizer/index.js
+++ src/components/Visualizer/index.js
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types'
 import styles from './styles.css'
 
 class Visualizer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.canvasRef = React.createRef()
+  }
+
+  componentDidMount() {
+    const graphicsContext = this.canvasRef.current.getContext('2d')
+  }
+
   render() {
     return (
       <div className={styles.root}>
-        <canvas className={styles.canvas} width="500" height="500">
+        <canvas ref={this.canvasRef} className={styles.canvas} width="500" height="500">
           Your browser does not support the HTML5 canvas element.
         </canvas>
       </div>
