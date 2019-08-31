949e5bac81554b27817b96827aaba7fe69ead632
PakkuDon <pakku.pd@gmail.com>
2019-09-01 00:37:31 +1000

    Delete Angular directives

--- src/directives/history.js
+++
@@ -1,8 +0,0 @@
-angular.module('scvDirectives').directive('history', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/views/history.html',
-        controller: 'HistoryController',
-        controllerAs: 'histCtrl'
-    };
-});
--- src/directives/notifications.js
+++
@@ -1,8 +0,0 @@
-angular.module('scvDirectives').directive('notifications', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/views/notifications.html',
-        controller: 'NotificationsController',
-        controllerAs: 'notificationsCtrl'
-    };
-});
