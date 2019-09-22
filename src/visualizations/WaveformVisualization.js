a22b21256eda2137b4a2e1fbab5db6c0fa6b578e
PakkuDon <pakku.pd@gmail.com>
2019-09-22 23:07:52 +1000

    Add waveform visualizer

---
+++ src/visualizations/WaveformVisualization.js
@@ -0,0 +1,24 @@
+export default {
+  draw: ({ waveformData, graphicsContext }) => {
+    const bufferLength = waveformData.length
+
+    const width = graphicsContext.canvas.width
+    const height = graphicsContext.canvas.height
+    const gridWidth = width / bufferLength
+    const gridHeight = height / 256
+    graphicsContext.clearRect(0, 0, width, height)
+    graphicsContext.beginPath()
+
+    graphicsContext.moveTo(0, gridHeight * waveformData[0])
+
+    for (let i = 1; i < bufferLength; i++) {
+      let x = i * gridWidth
+      let y = gridHeight * waveformData[i]
+
+      graphicsContext.lineTo(x, y)
+    }
+
+    graphicsContext.stroke()
+    graphicsContext.closePath()
+  },
+}
