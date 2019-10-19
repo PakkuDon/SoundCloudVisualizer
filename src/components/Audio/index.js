76b6c2653aba2a66d280b8efd731383f89f0265f
PakkuDon <pakku.pd@gmail.com>
2019-10-19 23:54:04 +1100

    Convert <Audio /> to a functional component using hooks

--- src/components/Audio/index.js
+++ src/components/Audio/index.js
@@ -1,43 +1,38 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import PropTypes from "prop-types"
 
-class Audio extends React.Component {
-  constructor(props) {
-    super(props)
-    this.audioRef = React.createRef()
-  }
+function Audio(props) {
+  const audioElement = useRef(null)
 
-  componentDidMount() {
+  useEffect(() => {
     const audioContext = new (window.AudioContext ||
       window.webkitAudioContext)()
     if (audioContext) {
       const source = audioContext.createMediaElementSource(
-        this.audioRef.current,
+        audioElement.current,
       )
       const analyser = audioContext.createAnalyser()
       source.connect(analyser)
       analyser.connect(audioContext.destination)
       analyser.fftSize = 256
-      this.props.onRender(analyser)
+      props.onRender(analyser)
     }
-  }
+  }, [])
 
-  render() {
-    const { onEnded, src } = this.props
+  const { onEnded, src } = props
 
-    return (
-      <audio
-        ref={this.audioRef}
-        src={src}
-        autoPlay
-        crossOrigin="anonymous"
-        controls
-        onEnded={onEnded}
-      >
-        Your browser does not support the HTML5 audio element.
-      </audio>
-    )
-  }
+  return (
+    <audio
+      ref={audioElement}
+      src={src}
+      autoPlay
+      crossOrigin="anonymous"
+      controls
+      onEnded={onEnded}
+    >
+      Your browser does not support the HTML5 audio element.
+    </audio>
+  )
 }
 
 Audio.propTypes = {
