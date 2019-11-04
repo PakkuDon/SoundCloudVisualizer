bea8dd34638f698ad418479448e2e24eea497d7b
PakkuDon <pakku.pd@gmail.com>
2019-11-04 23:53:56 +1100

    Allow SoundCloudClient to resolve playlists

--- src/SoundCloudClient.js
+++ src/SoundCloudClient.js
@@ -9,13 +9,28 @@ SoundCloudSDK.initialize({
 export default {
   resolve(inputUrl) {
     return SoundCloudSDK.resolve(inputUrl).then(response => {
-      const responseWithAuthorisedStreamUrl = {
-        ...response,
-        stream_url: `${response.stream_url}?client_id=${SOUNDCLOUD_CLIENT_ID}`,
+      if (response.kind === "track") {
+        const responseWithAuthorisedStreamUrl = {
+          ...response,
+          stream_url: `${response.stream_url}?client_id=${SOUNDCLOUD_CLIENT_ID}`,
+        }
+        return new Promise((resolve, reject) => {
+          resolve(responseWithAuthorisedStreamUrl)
+        })
+      } else if (response.kind === "playlist") {
+        const tracksWithAuthorisedStreamUrl = response.tracks.map(track => ({
+          ...track,
+          stream_url: `${track.stream_url}?client_id=${SOUNDCLOUD_CLIENT_ID}`,
+        }))
+
+        return new Promise((resolve, reject) => {
+          resolve(tracksWithAuthorisedStreamUrl)
+        })
+      } else {
+        throw new Error(
+          "Incompatible response type. Please enter either a track or playlist URL",
+        )
       }
-      return new Promise((resolve, reject) => {
-        resolve(responseWithAuthorisedStreamUrl)
-      })
     })
   },
 }
