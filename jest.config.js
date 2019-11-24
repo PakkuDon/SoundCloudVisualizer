85783d82e5e2fa3b11bfaf1dc78c5ecab29025ea
PakkuDon <pakku.pd@gmail.com>
2019-11-25 00:08:17 +1100

    Configure Jest to include Enzyme config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.css$": "identity-obj-proxy"
   },
+  setupFilesAfterEnv: ["./config/enzyme.config.js"],
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.js?(x)"],
 }
