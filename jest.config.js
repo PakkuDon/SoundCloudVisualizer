11f35562c1169b819162cc36c75cc89801a5376b
PakkuDon <pakku.pd@gmail.com>
2019-12-07 23:17:23 +1100

    Use enzyme-to-json to serialize snapshots

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "\\.css$": "identity-obj-proxy"
   },
   setupFilesAfterEnv: ["./config/enzyme.config.js"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
   testEnvironment: "node",
   testMatch: ["**/__test__/**/*.test.js"],
 }
