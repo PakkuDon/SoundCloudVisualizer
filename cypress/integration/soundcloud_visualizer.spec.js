d3be5806dfd143cb0507024cbe8afae5bd6b6e7c
PakkuDon <pakku.pd@gmail.com>
2020-01-03 23:45:09 +1100

    Write integration test to confirm loaded songs are added to history

--- cypress/integration/soundcloud_visualizer.spec.js
+++ cypress/integration/soundcloud_visualizer.spec.js
@@ -8,13 +8,27 @@ describe("SoundCloudVisualizer", () => {
   })
 
   describe("when user enters a SoundCloud track", () => {
-    it("plays selected song", () => {
-      cy.get("#track-input").type("https://soundcloud.com/dub-motion/dub-motion-awakened")
+    beforeEach(() => {
+      cy.get("#track-input").type(
+        "https://soundcloud.com/dub-motion/dub-motion-awakened",
+      )
       cy.contains("Load song").click()
       // TODO: Replace hard-coded wait with wait on SoundCloud resolution
       cy.wait(2000)
+    })
+
+    it("plays selected song", () => {
       // TODO: Stub response from SoundCloud API
-      cy.get("audio").should("have.attr", "src").should("contain", "https://api.soundcloud.com/tracks/42873590/stream")
+      cy.get("audio")
+        .should("have.attr", "src")
+        .should("contain", "https://api.soundcloud.com/tracks/42873590/stream")
+    })
+
+    it("adds song to history", () => {
+      // TODO: Stub response from SoundCloud API
+      cy.get("[data-testid='sidebar']")
+        .should("contain", "Dub Motion")
+        .should("contain", "Awakened")
     })
   })
 })
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ const Sidebar = props => {
   const [isOpen, toggleSidebar] = useState(true)
 
   return (
-    <div className={styles.root}>
+    <div className={styles.root} data-testid="sidebar">
       {isOpen && <div className={styles.sidebarWrapper}>{props.children}</div>}
       <button
         className={styles.toggleSidebar}
