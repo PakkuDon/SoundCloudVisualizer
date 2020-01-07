6f0be4bde918bed9d229432321f458f3a7c3cf53
PakkuDon <pakku.pd@gmail.com>
2020-01-07 23:41:44 +1100

    Add tests for case when user loads a song while one is currently playing

--- cypress/integration/soundcloud_visualizer.spec.js
+++ cypress/integration/soundcloud_visualizer.spec.js
@@ -17,18 +17,52 @@ describe("SoundCloudVisualizer", () => {
       cy.wait(2000)
     })
 
-    it("plays selected song", () => {
-      // TODO: Stub response from SoundCloud API
-      cy.get("audio")
-        .should("have.attr", "src")
-        .should("contain", "https://api.soundcloud.com/tracks/42873590/stream")
+    describe("and there is no song currently playing", () => {
+      it("plays selected song", () => {
+        // TODO: Stub response from SoundCloud API
+        cy.get("audio")
+          .should("have.attr", "src")
+          .should(
+            "contain",
+            "https://api.soundcloud.com/tracks/42873590/stream",
+          )
+      })
+
+      it("adds song to history", () => {
+        // TODO: Stub response from SoundCloud API
+        cy.get("[data-testid='sidebar']")
+          .should("contain", "Dub Motion")
+          .should("contain", "Awakened")
+      })
     })
 
-    it("adds song to history", () => {
-      // TODO: Stub response from SoundCloud API
-      cy.get("[data-testid='sidebar']")
-        .should("contain", "Dub Motion")
-        .should("contain", "Awakened")
+    describe("and there is a song currently playing", () => {
+      beforeEach(() => {
+        cy.get("#track-input")
+          .clear()
+          .type("https://soundcloud.com/monstercat/nitro-fun-new-game")
+        cy.contains("Load song").click()
+        // TODO: Replace hard-coded wait with wait on SoundCloud resolution
+        cy.wait(2000)
+      })
+
+      it("does not play input song", () => {
+        cy.get("audio")
+          .should("have.attr", "src")
+          .should(
+            "not.contain",
+            "https://api.soundcloud.com/tracks/133621063/stream",
+          )
+      })
+
+      it("adds song to playback queue", () => {
+        cy.get("[data-testid='sidebar']")
+          .contains("Playback queue")
+          .click()
+        cy.get("[data-testid='sidebar']")
+          .should("contain", "Nitro Fun - New Game")
+          .should("contain", "Monstercat")
+      })
     })
   })
 })
