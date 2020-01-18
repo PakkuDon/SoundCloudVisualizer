5363b19106186019cdb8ebee4993b48fbf3a588c
PakkuDon <pakku.pd@gmail.com>
2020-01-18 17:12:59 +1100

    Remove Enzyme
    Test suite has been ported over to @testing-library/react

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   moduleNameMapper: {
     "\\.css$": "identity-obj-proxy"
   },
-  setupFilesAfterEnv: ["./config/enzyme.config.js"],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
   testEnvironment: "jsdom",
   testMatch: ["**/__test__/**/*.test.js"],
 }
