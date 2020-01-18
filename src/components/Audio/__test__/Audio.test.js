92239f8235f229eb877daef8126b2067addd4243
PakkuDon <pakku.pd@gmail.com>
2020-01-18 16:11:56 +1100

    Rewrite <Audio /> tests using @testing-library/react

--- src/components/Audio/__test__/Audio.test.js
+++ src/components/Audio/__test__/Audio.test.js
@@ -1,22 +1,35 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { render, fireEvent } from "@testing-library/react"
 
 import Audio from "../Audio"
 
 describe("Audio", () => {
+  beforeEach(() => {
+    window.AudioContext = jest.fn().mockImplementation(() => {
+      return {
+        createMediaElementSource: () => ({ connect() {} }),
+        createAnalyser: () => ({ connect() {} }),
+      }
+    })
+  })
+
+  afterEach(() => {
+    window.AudioContext.mockRestore()
+  })
+
   it("uses provided src", () => {
     const src = "https://example.com/example.mp3"
-    const wrapper = shallow(<Audio src={src} />)
+    const { container } = render(<Audio src={src} />)
 
-    expect(wrapper).toMatchSnapshot()
+    expect(container).toMatchSnapshot()
   })
 
   describe("on audio end", () => {
     it("calls supplied onEnded handler", () => {
       const spy = jest.fn()
-      const wrapper = shallow(<Audio onEnded={spy} />)
+      const { container } = render(<Audio onEnded={spy} />)
 
-      wrapper.simulate("ended")
+      fireEvent.ended(container.querySelector("audio"))
 
       expect(spy).toBeCalled()
     })
