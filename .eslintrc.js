0e49dc4ae32c5b4e2f1d6d2ebe2c289a5662183f
PakkuDon <pakku.pd@gmail.com>
2020-02-03 00:19:42 +1100

    Mark cy as a readonly global in ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     ],
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "cy": "readonly",
     },
     "parserOptions": {
         "ecmaFeatures": {
