f7483cd2950940126a4b5b1b471cb057c0d0e371
PakkuDon <pakku.pd@gmail.com>
2020-05-16 23:14:48 +1000

    Change tabWidth in ESLint config to 2 spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest": true,
-        "node": true,
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest": true,
+    "node": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+    "cy": "readonly",
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly",
-        "cy": "readonly",
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "jsx-a11y",
-        "react",
-        "react-hooks"
-    ],
-    "rules": {
-        "jsx-a11y/media-has-caption": "off",
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
-    },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "jsx-a11y",
+    "react",
+    "react-hooks"
+  ],
+  "rules": {
+    "jsx-a11y/media-has-caption": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
     }
+  }
 };
